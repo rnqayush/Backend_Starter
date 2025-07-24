db652cde250998fcfd461c6e5b10de5fb1ae7520
Ayush Mishra <rnq.ayush@gmail.com>
2025-07-24 19:25:18 +0530

    code changes

--- src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-// Async handler to eliminate try-catch blocks
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-export default asyncHandler;
-
